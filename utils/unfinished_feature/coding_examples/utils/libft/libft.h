300de16df06066c0ce35b1ddf3927174203eb606
Turingraph <nemocheche@gmail.com>
2026-09-11 00:02:59 +0700

    Delete some outdated files

--- utils/unfinished_feature/coding_examples/utils/libft/libft.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIBFT_CODING_EXAMPLES_H
-# define LIBFT_CODING_EXAMPLES_H
-
-#include"../../../src/utils/libft/libft.h"
-
-#endif
--- utils/unfinished_feature/triangle/triangle_arr_example/triangle_arr.h
+++
@@ -1,7 +0,0 @@
-#ifndef TRIANGLE_ARR_CODING_EXAMPLES_H
-# define TRIANGLE_ARR_CODING_EXAMPLES_H
-
-#include"../../../src/graphic/triangle_arr/triangle_arr.h"
-#include"../../../src/input/table/table.h"
-
-#endif
