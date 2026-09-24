9916d3c0ce0663f8ef3ea37c97d6cd055adbd2e6
Turingraph <nemocheche@gmail.com>
2026-09-24 20:40:11 +0700

    Add another important lib

--- include/sunset442pm.h
+++ include/sunset442pm.h
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <stdbool.h>
 # include "MLX42.h"
+# include <fcntl.h>
 
 /* ************************************************************************** */
 /* src/utils/type/type.h */
